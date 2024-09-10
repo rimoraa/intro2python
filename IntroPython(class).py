792dea59f54b6a1f9bfb0ebe84111010fa64d42a
rimoraa <rm4248@columbia.edu>
2024-09-10 17:31:07 -0400

    Update IntroPython(class).py

---
+++ IntroPython(class).py
@@ -0,0 +1,65 @@
+# YOUR WORK GOES HERE!
+
+1. 5.0 float
+2. 5  int
+3. 5.0 float
+4. 2.6 float
+5. -2 int 
+
+
+## second ex:
+
+1. False
+2. True
+3. Error
+
+
+## ex with strings:
+
+1. foo\
+2. 4
+3. o
+4. error 
+5. \
+6. foo\foo\foo\bar
+7. foo\
+8. new excercise word
+
+##true and false ex
+
+y =  20
+if y > 10 or y <-10:
+    print("True")
+else: 
+    print("False")
+
+## If elif ex:
+my_number = 6
+
+if my_number <= 3:
+    print("less than or equal to 3")
+elif my_number > 3:
+    print("greater than 3")
+
+## if else ex:
+
+my_int = 3
+
+if type(my_int) == int:
+    print("your number is an integer")
+else:
+    print("your number is not an integer")
+
+## final ex:
+
+my_int = -11.2
+
+if type(my_int) == int:
+    if my_int < 0:
+        print("Negative")
+    elif my_int >= 0 and my_int <=10:
+        print("digit")
+    elif my_int >= 11:
+        print("We cant count this with our hands")
+else:
+    print("your number is not an integer")
